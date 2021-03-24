0914a4cb0071045f6a754e3756c1d3610d0387c0
draco.wang <goodbeam@163.com>
2021-03-24 10:38:27 +0800

    任务5-->路由传递参数

--- blockchain/app/__init__.py
+++ blockchain/app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,jsonify,url_for,redirect
 
 
 app = Flask(__name__)
@@ -8,12 +8,12 @@ app.config.from_object('config')
 @app.route('/index')
 def index():
     data = [1,3,4,5,6]
-
-
     return render_template('index.html',data=data)
 
 
-
+@app.route("/user/<user_name>")
+def user_info(user_name):
+    return f'hello {user_name}'
 
 
 
